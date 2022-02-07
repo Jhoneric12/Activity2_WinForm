ca11e3f9ac2c8ebbf4eeedce4b9c793879e60a61
jhoneric <jhonerix23@gmail.com>
2022-02-07 08:09:48 -0500

    first commit

---
+++ Activity2_WinForm/Activity2_WinForm/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace Activity2_WinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblFirstNum = new System.Windows.Forms.Label();
+            this.lblSecondNum = new System.Windows.Forms.Label();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.txtFirstNum = new System.Windows.Forms.TextBox();
+            this.txtSecondNum = new System.Windows.Forms.TextBox();
+            this.txtToTal = new System.Windows.Forms.TextBox();
+            this.btnDiff = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnSum = new System.Windows.Forms.Button();
+            this.btnProduct = new System.Windows.Forms.Button();
+            this.btnQoutient = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblFirstNum
+            // 
+            this.lblFirstNum.AutoSize = true;
+            this.lblFirstNum.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.lblFirstNum.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblFirstNum.Font = new System.Drawing.Font("MS Reference Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFirstNum.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.lblFirstNum.Location = new System.Drawing.Point(179, 106);
+            this.lblFirstNum.Name = "lblFirstNum";
+            this.lblFirstNum.Size = new System.Drawing.Size(218, 29);
+            this.lblFirstNum.TabIndex = 0;
+            this.lblFirstNum.Text = "FIRST NUMBER ";
+            // 
+            // lblSecondNum
+            // 
+            this.lblSecondNum.AutoSize = true;
+            this.lblSecondNum.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.lblSecondNum.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblSecondNum.Font = new System.Drawing.Font("MS Reference Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSecondNum.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.lblSecondNum.Location = new System.Drawing.Point(179, 179);
+            this.lblSecondNum.Name = "lblSecondNum";
+            this.lblSecondNum.Size = new System.Drawing.Size(251, 29);
+            this.lblSecondNum.TabIndex = 1;
+            this.lblSecondNum.Text = "SECOND NUMBER ";
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.lblAnswer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblAnswer.Font = new System.Drawing.Font("MS Reference Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswer.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.lblAnswer.Location = new System.Drawing.Point(179, 250);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(125, 29);
+            this.lblAnswer.TabIndex = 2;
+            this.lblAnswer.Text = "ANSWER";
+            // 
+            // txtFirstNum
+            // 
+            this.txtFirstNum.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.txtFirstNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFirstNum.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.txtFirstNum.Location = new System.Drawing.Point(501, 106);
+            this.txtFirstNum.Name = "txtFirstNum";
+            this.txtFirstNum.Size = new System.Drawing.Size(439, 34);
+            this.txtFirstNum.TabIndex = 0;
+            this.txtFirstNum.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtSecondNum
+            // 
+            this.txtSecondNum.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.txtSecondNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSecondNum.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.txtSecondNum.Location = new System.Drawing.Point(501, 174);
+            this.txtSecondNum.Name = "txtSecondNum";
+            this.txtSecondNum.Size = new System.Drawing.Size(439, 34);
+            this.txtSecondNum.TabIndex = 1;
+            this.txtSecondNum.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtToTal
+            // 
+            this.txtToTal.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.txtToTal.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtToTal.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.txtToTal.Location = new System.Drawing.Point(501, 247);
+            this.txtToTal.Name = "txtToTal";
+            this.txtToTal.ReadOnly = true;
+            this.txtToTal.Size = new System.Drawing.Size(439, 34);
+            this.txtToTal.TabIndex = 1;
+            this.txtToTal.TabStop = false;
+            this.txtToTal.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnDiff
+            // 
+            this.btnDiff.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.btnDiff.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDiff.Font = new System.Drawing.Font("MS Reference Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDiff.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnDiff.Location = new System.Drawing.Point(184, 518);
+            this.btnDiff.Name = "btnDiff";
+            this.btnDiff.Size = new System.Drawing.Size(234, 68);
+            this.btnDiff.TabIndex = 3;
+            this.btnDiff.Text = "DIFFERENCE";
+            this.btnDiff.UseVisualStyleBackColor = false;
+            this.btnDiff.Click += new System.EventHandler(this.btnDiff_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClear.Font = new System.Drawing.Font("MS Reference Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnClear.Location = new System.Drawing.Point(473, 465);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(182, 68);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "CLEAR";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnSum
+            // 
+            this.btnSum.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.btnSum.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSum.Font = new System.Drawing.Font("MS Reference Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSum.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnSum.Location = new System.Drawing.Point(184, 412);
+            this.btnSum.Name = "btnSum";
+            this.btnSum.Size = new System.Drawing.Size(234, 68);
+            this.btnSum.TabIndex = 2;
+            this.btnSum.Text = "SUM";
+            this.btnSum.UseVisualStyleBackColor = false;
+            this.btnSum.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnProduct
+            // 
+            this.btnProduct.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.btnProduct.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnProduct.Font = new System.Drawing.Font("MS Reference Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProduct.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnProduct.Location = new System.Drawing.Point(706, 412);
+            this.btnProduct.Name = "btnProduct";
+            this.btnProduct.Size = new System.Drawing.Size(234, 68);
+            this.btnProduct.TabIndex = 4;
+            this.btnProduct.Text = "PRODUCT";
+            this.btnProduct.UseVisualStyleBackColor = false;
+            this.btnProduct.Click += new System.EventHandler(this.btnProduct_Click);
+            // 
+            // btnQoutient
+            // 
+            this.btnQoutient.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.btnQoutient.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnQoutient.Font = new System.Drawing.Font("MS Reference Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnQoutient.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnQoutient.Location = new System.Drawing.Point(706, 518);
+            this.btnQoutient.Name = "btnQoutient";
+            this.btnQoutient.Size = new System.Drawing.Size(234, 68);
+            this.btnQoutient.TabIndex = 5;
+            this.btnQoutient.Text = "QOUTIENT";
+            this.btnQoutient.UseVisualStyleBackColor = false;
+            this.btnQoutient.Click += new System.EventHandler(this.btnQoutient_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.ClientSize = new System.Drawing.Size(1111, 757);
+            this.Controls.Add(this.btnQoutient);
+            this.Controls.Add(this.btnProduct);
+            this.Controls.Add(this.btnSum);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnDiff);
+            this.Controls.Add(this.txtToTal);
+            this.Controls.Add(this.txtSecondNum);
+            this.Controls.Add(this.txtFirstNum);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.lblSecondNum);
+            this.Controls.Add(this.lblFirstNum);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblFirstNum;
+        private System.Windows.Forms.Label lblSecondNum;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.TextBox txtFirstNum;
+        private System.Windows.Forms.TextBox txtSecondNum;
+        private System.Windows.Forms.TextBox txtToTal;
+        private System.Windows.Forms.Button btnDiff;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnSum;
+        private System.Windows.Forms.Button btnProduct;
+        private System.Windows.Forms.Button btnQoutient;
+    }
+}
+
---
+++ Activity2_WinForm/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity2_WinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Activity # 2";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // FOR SUM
+            txtToTal.Text = (Convert.ToDouble(txtFirstNum.Text) + Convert.ToDouble(txtSecondNum.Text)).ToString();
+        }
+
+        private void btnDiff_Click(object sender, EventArgs e)
+        {
+            // FOR DIFFERENCE
+            txtToTal.Text = (Convert.ToDouble(txtFirstNum.Text) - Convert.ToDouble(txtSecondNum.Text)).ToString();
+        }
+
+        private void btnProduct_Click(object sender, EventArgs e)
+        {
+            // FOR PRODUCT
+            txtToTal.Text = (Convert.ToDouble(txtFirstNum.Text) * Convert.ToDouble(txtSecondNum.Text)).ToString();
+        }
+
+        private void btnQoutient_Click(object sender, EventArgs e)
+        {
+            // FOR QOUTIENT
+            txtToTal.Text = (Convert.ToDouble(txtFirstNum.Text) / Convert.ToDouble(txtSecondNum.Text)).ToString();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            // FOR CLEAR
+            txtFirstNum.Clear();
+            txtSecondNum.Clear();
+            txtToTal.Clear();
+        }
+    }
+}
